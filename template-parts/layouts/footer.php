84c15ba3d105d4c6c7484522bc44a625c3245ed2
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-28 11:53:51 +0700

    feat: add layout footer

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,183 @@
+    <!--footer-->
+    <div class="footer">
+        <!--footer__top-->
+        <div class="footer__top uk-overflow-hidden" uk-scrollspy="cls: uk-animation-slide-bottom-small; target: .footer__top__img; delay: 300; repeat: true">
+            <div class="uk-container">
+                <div class="uk-grid-small uk-grid-33-m uk-flex-bottom" uk-grid>
+                    <div class="uk-width-auto@m uk-visible@m">
+                        <img class="footer__top__img" src="images/Group3880.png" alt="">
+                    </div>
+                    <div class="uk-width-expand">
+                        <div class="footer__top__section">
+                            <div class="uk-grid-24 uk-grid-62-m uk-flex-middle" uk-grid>
+                                <div class="uk-width-auto@m">
+                                    <div class="footer__top__w">
+                                        <h3  class="uk-h3 footer__top__title">Liên hệ với chúng tôi</h3>
+                                        <div class="footer__top__desc">Nhập sô điện thoại của bạn để chúng tôi có thể tư vấn tốt hơn</div>
+                                    </div>
+                                </div>
+                                <div class="uk-width-expand">
+                                    <div class="uk-grid-10 uk-grid-16-m" uk-grid>
+                                        <div class="uk-width-expand">
+                                            <div class="uk-inline uk-width-1-1">
+                                                <span class="uk-form-icon" uk-icon="icon: user"></span>
+                                                <input class="uk-input footer__top__inputRegister" type="tel" placeholder="Số điện thoại">
+                                            </div>
+                                        </div>
+                                        <div class="uk-width-auto">
+                                            <button class="uk-button footer__top__btnSubmit uk-button-secondary" data-mb="." data-pc="Gửi"></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/footer__top-->
+
+        <!--footer__bottom-->
+        <div class="footer__bottom">
+            <div class="uk-container">
+                <div class="uk-grid uk-grid-20 uk-grid-101-m" uk-grid>
+                    <div class="uk-width-auto@m">
+                        <div class="footer__bottom__width">
+                            <div class="uk-child-width-auto uk-child-width-1-1@m uk-grid-20 uk-flex-between uk-flex-middle" uk-grid>
+                                <div>
+                                    <figure class="footer__bottom__logo uk-display-inline-block"><img src="images/logo.png" alt=""></figure>
+                                </div>
+                                <div>
+                                    <div class="uk-grid uk-grid-16 uk-child-width-auto">
+                                        <div><a href="" class="footer__bottom__socialIcon footer__bottom__socialIcon--youtube"></a></div>
+                                        <div><a href="" class="footer__bottom__socialIcon footer__bottom__socialIcon--fb"></a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-width-expand">
+                        <ul class="uk-hidden@m" uk-accordion="multiple: true">
+                            <li>
+                                <a class="uk-accordion-title" href="#">VỀ SIMPAY</a>
+                                <div class="uk-accordion-content">
+                                    <ul class="uk-list footer__bottom__menuList">
+                                        <li><a href="">Giới thiệu về Simpay</a></li>
+                                        <li><a href="">Tuyển dụng</a></li>
+                                        <li><a href="">Chính sách bảo mật</a></li>
+                                        <li><a href="">Điều khoản sử dụng</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="uk-accordion-title" href="#">HỖ TRỢ KHÁCH HÀNG</a>
+                                <div class="uk-accordion-content">
+                                    <ul class="uk-list footer__bottom__menuList">
+                                        <li><a href="">Hottline chăm sóc khách hàng: 1900 4750</a></li>
+                                        <li><a href="">Mail hỗ trợ: cskh@simpay.vn</a></li>
+                                        <li><a href="">Câu hỏi thường gặp</a></li>
+                                        <li><a href="">Hướng dẫn trả góp</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="uk-open">
+                                <a class="uk-accordion-title" href="#">PHƯƠNG THỨC THANH TOÁN</a>
+                                <div class="uk-accordion-content">
+                                    <div class="uk-child-width-auto uk-grid-12" uk-grid>
+                                        <div><img src="images/visa.png" alt=""></div>
+                                        <div><img src="images/mastercard.png" alt=""></div>
+                                        <div><img src="images/the.png" alt=""></div>
+                                        <div><img src="images/tienmat.png" alt=""></div>
+                                        <div><img src="images/tragop.png" alt=""></div>
+                                        <div><img src="images/mastercard.png" alt=""></div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <a class="uk-accordion-title" href="#">TRỤ SỞ CHÍNH</a>
+                                <div class="uk-accordion-content">
+                                    <ul class="uk-list footer__bottom__menuList">
+                                        <li>Địa chỉ: Tầng 2, Tòa nhà 78 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội</li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="uk-child-width-auto@m uk-visible@m" uk-grid>
+                            <div>
+                                <h4 class="uk-h4 footer__bottom__title">VỀ SIMPAY</h4>
+                                <ul class="uk-list footer__bottom__menuList">
+                                    <li><a href="">Giới thiệu về Simpay</a></li>
+                                    <li><a href="">Tuyển dụng</a></li>
+                                    <li><a href="">Chính sách bảo mật</a></li>
+                                    <li><a href="">Điều khoản sử dụng</a></li>
+                                </ul>
+                            </div>
+                            <div class="uk-width-1-3@m">
+                                <h4 class="uk-h4 footer__bottom__title">HỖ TRỢ KHÁCH HÀNG</h4>
+                                <ul class="uk-list footer__bottom__menuList">
+                                    <li><a href="">Hottline chăm sóc khách hàng: 1900 4750</a></li>
+                                    <li><a href="">Mail hỗ trợ: cskh@simpay.vn</a></li>
+                                    <li><a href="">Câu hỏi thường gặp</a></li>
+                                    <li><a href="">Hướng dẫn trả góp</a></li>
+                                </ul>
+                            </div>
+                            <div class="uk-width-1-4@m">
+                                <h4 class="uk-h4 footer__bottom__title">PHƯƠNG THỨC THANH TOÁN</h4>
+                                <div class="uk-child-width-auto uk-grid-12" uk-grid>
+                                    <div><img src="images/visa.png" alt=""></div>
+                                    <div><img src="images/mastercard.png" alt=""></div>
+                                    <div><img src="images/the.png" alt=""></div>
+                                    <div><img src="images/tienmat.png" alt=""></div>
+                                    <div><img src="images/tragop.png" alt=""></div>
+                                    <div><img src="images/mastercard.png" alt=""></div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="footer__bottom__width1">
+                                    <h4 class="uk-h4 footer__bottom__title">TRỤ SỞ CHÍNH</h4>
+                                    <ul class="uk-list footer__bottom__menuList">
+                                        <li>Địa chỉ: Tầng 2, Tòa nhà 78 Trần Hưng Đạo, Hoàn Kiếm, Hà Nội</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="footer__sectionBaohanh uk-section-xsmall uk-visible@m">
+                            <div class="uk-child-width-auto uk-grid-divider" uk-grid>
+                                <div>
+                                    <div class="uk-flex-middle uk-grid-48-m" uk-grid>
+                                        <div>
+                                            <h4 class="uk-h4 footer__sectionBaohanh__title">Hỗ trợ đại lý</h4>
+                                        </div>
+                                        <div>
+                                            <div class="footer__sectionBaohanh__desc">0988 855 155</div>
+                                            <div class="footer__sectionBaohanh__desc">daily@simpay.vn</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="uk-flex-middle uk-grid-48-m" uk-grid>
+                                        <div>
+                                            <h4 class="uk-h4 footer__sectionBaohanh__title">Hỗ trợ đại lý</h4>
+                                        </div>
+                                        <div>
+                                            <div class="footer__sectionBaohanh__desc">0979 537 195</div>
+                                            <div class="footer__sectionBaohanh__desc">tuyendung@simpay.vn</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="uk-section-xsmall footer__sectionCopyright">
+                    <div class="footer__sectionCopyright__title">© 2021 - Bản quyền của Công ty SIMPAY</div>
+                    <div class="footer__sectionCopyright__desc">Giấy chứng nhận Đăng ký kinh doanh số 0000000000 do Sở Kế hoạch và Đầu tư Thành Phố Hà Nội cấp ngày 10/10/2020</div>
+                </div>
+            </div>
+        </div>
+        <!--/footer__bottom-->
+    </div>
+    <!--/footer-->
 </div>
 <!--/app-->
 </body>
