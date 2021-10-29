238fe5504771ae91010146162c8440acb7c36d1c
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-10-29 11:45:43 +0700

    feat: add pagination

--- index.php
+++ index.php
@@ -228,6 +228,16 @@ $isCart = rand(false,true);
                             </div>
                         </div>
 
+                        <ul class="uk-pagination uk-flex-center pagination uk-grid-4" uk-margin>
+                            <li><a href="#"><span uk-pagination-previous></span></a></li>
+                            <li><a href="#">1</a></li>
+                            <li class="uk-disabled"><span>...</span></li>
+                            <li><a href="#">5</a></li>
+                            <li><a href="#">6</a></li>
+                            <li class="uk-active"><span>7</span></li>
+                            <li><a href="#">8</a></li>
+                            <li><a href="#"><span uk-pagination-next></span></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
